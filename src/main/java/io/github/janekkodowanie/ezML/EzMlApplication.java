ad7f0e24bd65fe29d174cb63f70109d58fa3dd32
WegielWBucie <pondroszejset@gmail.com>
2023-06-26 20:41:40 +0200

    Init.

---
+++ src/main/java/io/github/janekkodowanie/ezML/EzMlApplication.java
@@ -0,0 +1,13 @@
+package io.github.janekkodowanie.ezML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzMlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/janekkodowanie/ezML/EzMlApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.janekkodowanie.ezML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzMlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
